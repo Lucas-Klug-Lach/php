db814bfa1aed34f958c586efde70dffcc8f82f6d
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2024-08-02 10:26:30 -0300

    Create ex003

---
+++ exercícios/ex003/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // $num = 0b10101;
+        // print "O valor da variavel é $num";
+
+        // $v = 300;
+        // var_dump($v)
+
+        //  $num = 3e2;
+        //  print "O valor é $num";
+
+        //   $num = (int) "950";
+        //   var_dump($num);
+        
+        //   $casado = true;
+        //   var_dump($casado);
+        //   print "O valor para casado é $casado";
+
+        //  $vet = [9, 0, 9, 8, 7];
+        //  var_dump($vet);
+
+        // class pessoa{
+        //     private string $nome;
+        // }
+
+        // $p = new pessoa;
+        // var_dump($p);
+    ?>
+</body>
+</html>
